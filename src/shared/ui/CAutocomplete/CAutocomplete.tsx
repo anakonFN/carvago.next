67ee089d9568303141f401899e82ef326f27e8b5
anakon73 <nazarkinah73@gmail.com>
2023-04-27 15:49:10 +0300

    fix(CAutocomplete): increase z-index of combobox options

--- src/shared/ui/CAutocomplete/CAutocomplete.tsx
+++ src/shared/ui/CAutocomplete/CAutocomplete.tsx
@@ -86,7 +86,7 @@ export function CAutocomplete<T extends Option>({
               >
                   <Combobox.Options
                       className="
-                      absolute z-10 mt-1 max-h-60
+                      absolute z-[101] mt-1 max-h-60
                       w-full overflow-auto rounded-md bg-white py-1
                       text-base shadow-lg ring-1
                       ring-black/5 focus:outline-none sm:text-sm
