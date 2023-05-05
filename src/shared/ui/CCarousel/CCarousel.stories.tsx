15da92fd34f5c43a00867acedb60b5365cb3d315
anakon73 <nazarkinah73@gmail.com>
2023-05-05 13:40:37 +0300

    docs(CCarousel): pass array of objects instead of array of strings

--- src/shared/ui/CCarousel/CCarousel.stories.tsx
+++ src/shared/ui/CCarousel/CCarousel.stories.tsx
@@ -11,16 +11,76 @@ const meta: Meta<CCarouselProps> = {
 export default meta
 
 const images = [
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img1.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img2.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img3.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img4.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img5.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img6.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img7.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img8.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img9.jpg',
-  'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img10.jpg',
+  {
+    id: 1,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img1.jpg',
+    ordering: 1,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 2,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img2.jpg',
+    ordering: 2,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 3,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img3.jpg',
+    ordering: 3,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 4,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img4.jpg',
+    ordering: 4,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 5,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img5.jpg',
+    ordering: 5,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 6,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img6.jpg',
+    ordering: 6,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 7,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img7.jpg',
+    ordering: 7,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 8,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img8.jpg',
+    ordering: 8,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 9,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img9.jpg',
+    ordering: 9,
+    ordering_computed: undefined,
+    variants: [],
+  },
+  {
+    id: 10,
+    path: 'https://bidfax.info/uploads/posts/2023-01/05/mclaren-automotive-720s-2019-sbm14dca7kw003067-img10.jpg',
+    ordering: 10,
+    ordering_computed: undefined,
+    variants: [],
+  },
 ]
 
 export function Default() {
