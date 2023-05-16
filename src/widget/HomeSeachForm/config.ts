31bc929440895a5af69ef73091e0527a988f22d7
anakon73 <nazarkinah73@gmail.com>
2023-05-16 17:12:32 +0300

    refactor(HomeSearchForm): update objects with data of price, data, etc

--- src/widget/HomeSeachForm/config.ts
+++ src/widget/HomeSeachForm/config.ts
@@ -1,52 +1,73 @@
-export interface Option {
+export const price: {
   key: number
   label: string
-}
-
-export const carBrands: Option[] = [
-  { key: 1, label: 'Mercedes-benz' },
-  { key: 2, label: 'Tesla' },
-  { key: 3, label: 'Audi' },
-  { key: 4, label: 'BMW' },
-  { key: 5, label: 'McLaren' },
-  { key: 6, label: 'Ford' },
-  { key: 7, label: 'Mazda' },
-  { key: 8, label: 'Hennesey' },
+}[] = [
+  { key: 200000, label: '2000' },
+  { key: 300000, label: '3000' },
+  { key: 400000, label: '4000' },
+  { key: 500000, label: '5000' },
+  { key: 600000, label: '6000' },
+  { key: 700000, label: '7000' },
+  { key: 800000, label: '8000' },
+  { key: 1000000, label: '10000' },
+  { key: 1250000, label: '12500' },
+  { key: 1500000, label: '15000' },
+  { key: 1750000, label: '17500' },
+  { key: 2000000, label: '20000' },
+  { key: 2500000, label: '25000' },
+  { key: 3000000, label: '30000' },
+  { key: 3500000, label: '35000' },
+  { key: 4000000, label: '40000' },
+  { key: 5000000, label: '50000' },
+  { key: 7500000, label: '75000' },
+  { key: 10000000, label: '100000' },
+  { key: 15000000, label: '150000' },
 ]
-
-export const mileage: Option[] = [
-  { key: 1, label: 'Mileage' },
-  { key: 2, label: '2500' },
-  { key: 3, label: '5000' },
-  { key: 4, label: '10000' },
-  { key: 5, label: '15000' },
-  { key: 6, label: '20000' },
-  { key: 7, label: '25000' },
-  { key: 8, label: '30000' },
+export const registerDate: {
+  key: number
+  label: string
+}[] = [
+  { key: 2023, label: '2023' },
+  { key: 2022, label: '2022' },
+  { key: 2021, label: '2021' },
+  { key: 2020, label: '2020' },
+  { key: 2019, label: '2019' },
+  { key: 2018, label: '2018' },
+  { key: 2017, label: '2017' },
+  { key: 2016, label: '2016' },
+  { key: 2015, label: '2015' },
+  { key: 2014, label: '2014' },
+  { key: 2013, label: '2013' },
+  { key: 2012, label: '2012' },
+  { key: 2011, label: '2011' },
+  { key: 2010, label: '2010' },
+  { key: 2009, label: '2009' },
+  { key: 2008, label: '2008' },
+  { key: 2007, label: '2007' },
+  { key: 2006, label: '2006' },
+  { key: 2005, label: '2005' },
+  { key: 2004, label: '2004' },
+  { key: 2003, label: '2003' },
 ]
-
-export const registerDate: Option[] = [
-  { key: 1, label: 'any' },
-  { key: 2, label: '2023' },
-  { key: 3, label: '2022' },
-  { key: 4, label: '2021' },
-  { key: 5, label: '2020' },
-  { key: 6, label: '2019' },
-  { key: 7, label: '2018' },
-  { key: 8, label: '2017' },
-  { key: 9, label: '2016' },
-  { key: 10, label: '2015' },
-]
-
-export const price: Option[] = [
-  { key: 1, label: '2000' },
-  { key: 2, label: '3000' },
-  { key: 3, label: '4000' },
-  { key: 4, label: '5000' },
-  { key: 5, label: '6000' },
-  { key: 6, label: '7000' },
-  { key: 7, label: '8000' },
-  { key: 8, label: '10000' },
-  { key: 9, label: '12500' },
-  { key: 10, label: '15000' },
+export const kmsDriven: {
+  key: number
+  label: string
+}[] = [
+  { key: 2500, label: '2500' },
+  { key: 5000, label: '5000' },
+  { key: 10000, label: '10000' },
+  { key: 15000, label: '15000' },
+  { key: 20000, label: '20000' },
+  { key: 30000, label: '30000' },
+  { key: 40000, label: '40000' },
+  { key: 50000, label: '50000' },
+  { key: 60000, label: '60000' },
+  { key: 70000, label: '70000' },
+  { key: 80000, label: '80000' },
+  { key: 90000, label: '90000' },
+  { key: 100000, label: '100000' },
+  { key: 125000, label: '125000' },
+  { key: 150000, label: '150000' },
+  { key: 175000, label: '175000' },
+  { key: 200000, label: '200000' },
 ]
