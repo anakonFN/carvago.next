f483dde6d452c451f4aa35962670ad93b3f18df7
anakon73 <nazarkinah73@gmail.com>
2023-05-19 20:01:23 +0300

    feat(API): off refetchOnWindowFocus

--- src/shared/api/vehicle/hooks.ts
+++ src/shared/api/vehicle/hooks.ts
@@ -4,5 +4,6 @@ import { VehicleFetcher, vehicleKeys } from './query'
 export function useVehicle(carId: string) {
   return useQuery(vehicleKeys.GetVehicle, () => VehicleFetcher(carId), {
     keepPreviousData: false,
+    refetchOnWindowFocus: false,
   })
 }
