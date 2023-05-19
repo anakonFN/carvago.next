061a440ca7ad0486a6c778247829b933b6b94453
anakon73 <nazarkinah73@gmail.com>
2023-05-19 19:30:57 +0300

    feat(404): add title

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,30 +1,40 @@
-import { CButton } from '@/shared/ui/CButton'
+import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { CButton } from '@/shared/ui/CButton'
+
 export default function NotFoundPage() {
   return (
-      <div className='flex flex-col items-center pt-12'>
-          <Image
-              alt='404'
-              className='rounded-lg'
-              height={300}
-              src="/404.webp"
-              width={400}
-          />
+      <>
+          <Head>
+              <title>
+                  Carvago | 404
+              </title>
+          </Head>
 
-          <div className='py-8 text-center text-4xl font-bold'>
-              You have reached a non-existent page
+          <div className='flex flex-col items-center pt-12'>
+              <Image
+                  alt='404'
+                  className='rounded-lg'
+                  height={300}
+                  src="/404.webp"
+                  width={400}
+              />
+
+              <div className='py-8 text-center text-4xl font-bold'>
+                  You have reached a non-existent page
+              </div>
+
+              <CButton
+                  size='sm'
+                  variant='primary'
+              >
+                  <Link href="/">
+                      Go back
+                  </Link>
+              </CButton>
           </div>
-
-          <CButton
-              size='sm'
-              variant='primary'
-          >
-              <Link href="/">
-                  Go back
-              </Link>
-          </CButton>
-      </div>
+      </>
   )
 }
